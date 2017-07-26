cd99d7b8d1f236962104f3216561acb671dfbed0
Vai Mor <mor.vaibhav577@gmail.com>
2017-07-26 17:18:31 +1200

    Goals page complete

--- js/goals.js
+++ js/goals.js
@@ -1,29 +1,51 @@
-// Global Variables
-var driving = false;
-var sideBarOpen = false;
+$(document).ready(function() {
+    var open = $('.open-nav'),
+        overlay = $('.overlay'),
+        close = $('.close');
 
-function openSlideMenu() {
-  sideBarOpen = true;
-  document.getElementById('side-menu').style.width = '250px';
-  document.querySelector('.goals-background').classList.add("overlay");
-  $('.goals-background').click(closeSlideMenu);
-}
+    open.click(function() {
+        overlay.show();
+        $('#wrapper').addClass('toggled');
+    });
 
-function closeSlideMenu() {
-  sideBarOpen = false;
-  document.getElementById('side-menu').style.width = '0';
-  document.querySelector('.goals-background ').classList.remove("overlay");
-}
+    overlay.click(function() {
+      overlay.hide();
+      $('#wrapper').removeClass('toggled');
+    })
+});
 
-function startJourney() {
-  if (sideBarOpen === false) {
-    if (driving === false) {
-      document.querySelector('.fa, .fa-car').style.color = '#b00';
-      driving = true;
-    }
-    else {
-      document.querySelector('.fa, .fa-car').style.color = '#008900';
-      driving = false;
-    }
-  }
-}
+$(document).ready(function() {
+    $('input[name="speedingGoal"]').change(function() {
+      var value = document.querySelector('input[name="speedingGoal"]:checked').value;
+      if (value === "easy") {
+        document.querySelector('.speedingFace').style.color = 'yellow';
+        document.querySelector('.speedingFire').style.color = 'yellow';
+      }
+      else if (value === "medium") {
+        document.querySelector('.speedingFace').style.color = 'orange';
+        document.querySelector('.speedingFire').style.color = 'orange';
+      }
+      else if (value === "hard") {
+        document.querySelector('.speedingFace').style.color = 'red';
+        document.querySelector('.speedingFire').style.color = 'red';
+      }
+    });
+});
+
+$(document).ready(function() {
+    $('input[name="smoothnessGoal"]').change(function() {
+      var value = document.querySelector('input[name="smoothnessGoal"]:checked').value;
+      if (value === "easy") {
+        document.querySelector('.smoothnessFace').style.color = 'yellow';
+        document.querySelector('.smoothnessFire').style.color = 'yellow';
+      }
+      else if (value === "medium") {
+        document.querySelector('.smoothnessFace').style.color = 'orange';
+        document.querySelector('.smoothnessFire').style.color = 'orange';
+      }
+      else if (value === "hard") {
+        document.querySelector('.smoothnessFace').style.color = 'red';
+        document.querySelector('.smoothnessFire').style.color = 'red';
+      }
+    });
+});
